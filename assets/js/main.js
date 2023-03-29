9de284f0d2476b0041ac9abf2b83f9f0d74751f6
rukhsar624 <rukhsar56@gmail.com>
2023-03-29 14:48:50 -0700

    add code from home

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,3 +211,71 @@ var swiper = new Swiper(".mySwiper1", {
   
   });
   // 
+  // Swiper Slider2
+var swiper = new Swiper(".mySwiper2", {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	loop:true,
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+	breakpoints: {
+	  // when window width is >= 320px
+	  768: {
+		slidesPerView: 3,
+		spaceBetween: 20
+	  },
+	  375: {
+		slidesPerView: 1,
+		spaceBetween: 20
+	  },
+	  320: {
+		slidesPerView: 1,
+		spaceBetween: 20
+	  },
+	  280: {
+		slidesPerView: 1,
+		spaceBetween: 20
+	  },
+	  414: {
+		slidesPerView: 2,
+		spaceBetween: 20
+	  },
+	  1920: {
+		slidesPerView: 3,
+		spaceBetween: 20
+	  },
+	  1600: {
+		slidesPerView: 3,
+		spaceBetween: 20
+  
+	  },
+	  1366: {
+		slidesPerView: 3,
+		spaceBetween: 10
+	  },
+	  1400: {
+		slidesPerView: 3,
+		spaceBetween: 10
+	  },
+    1366: {
+      slidesPerView: 3,
+      spaceBetween: 10
+      },
+      2560: {
+        slidesPerView: 5,
+        spaceBetween: 20
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 20
+          }
+	}
+  
+  });
+  // 
